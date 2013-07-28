6751e75353c74bb46ebe5eac36d0c5a69691db2e
joyjding <joy.j.ding@gmail.com>
2013-07-28 16:24:40 -0700

    refactored code from last time

--- 10KTimer/10KTimer/JDViewController.m
+++ 10KTimer/10KTimer/JDViewController.m
@@ -21,7 +21,6 @@
 - (void)viewDidLoad //before view loads the first time, do something
 {
     [super viewDidLoad];
-    
 }
 
 
@@ -45,7 +44,7 @@
     }    
 }
 
-
+//function that ns timer could call, that would create a block every s seconds, and change level every 5 blocks
 
 
 - (void) viewWillDisappear:(BOOL)animated //what happens when you navigate away from the view
@@ -82,7 +81,7 @@
 //        [NSTimer scheduledTimerWithTimeInterval:5.0f
 //                                         target:self selector:@selector(createBlockAndAnimate:counter)
 //                                       userInfo:nil
-//                                        repeats:YES]; //How do I pass an argument into this timer object?
+//                                        repeats:YES]; //How do I pass an argument into this timer object? Using userInfo
 //    }
 //}
 
@@ -95,43 +94,51 @@
     return block;
 }
 
+
+- (UIColor*)createRandomColor //generate a random color *color
+{
+    CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
+    CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
+    CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
+    UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
+    return color;
+}
+
+
 - (void) createBlockAndAnimate: (int)level
-    {
-        
-        NSInteger randStart = arc4random_uniform(5); //generate a random start position
-        NSInteger randEnd = arc4random_uniform(5); //generate a random end position
-        
-        //generate a random color *color
-        CGFloat hue = ( arc4random() % 256 / 256.0 );  //  0.0 to 1.0
-        CGFloat saturation = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from white
-        CGFloat brightness = ( arc4random() % 128 / 256.0 ) + 0.5;  //  0.5 to 1.0, away from black
-        UIColor *color = [UIColor colorWithHue:hue saturation:saturation brightness:brightness alpha:1];
-        
-        //create block of color *color
-        UIView* newBlock =[self createBlock:color]; //Main view: create this block, doesn't show until added in subview
-        newBlock.frame=CGRectMake(kBlockSize*randStart, 0.0f, kBlockSize, kBlockSize);//(x pos, y pos, width, height)
-        
-        [self.view addSubview:newBlock]; //add block to main view (show block)
-        
-        //animate the block
-        [UIView animateWithDuration:10.0f
-                              delay:0.5f
-                            options:UIViewAnimationOptionCurveLinear
-                         animations:^(){
-                                newBlock.frame=CGRectMake(randEnd*CGRectGetWidth(newBlock.frame), CGRectGetHeight(newBlock.superview.bounds)-level*CGRectGetHeight(newBlock.frame), 64.0f, 64.0f); //QUESTION: Why does this slip off the screen?
-                        
-                                                       
-                                                       //kBlockSize*randEnd, (self.view.frame.size.height-(level*kBlockSize)), newBlock.frame.size.width, newBlock.frame.size.height); //weird off by a lot error going on here
-                         }
-                         completion:^(BOOL finished) {
-                             //NSString frameHeight= self.frame.size.height;
-                             //NSLog(frameHeight);
-                             newBlock.bounds = newBlock.bounds;
-                             NSLog(@"dropped block %@", NSStringFromCGRect(newBlock.bounds)); //@ for any NS object
-                             //NSLog(@"dropped block", frameHeight);
-                         }
-         ];
-        }
+{
+    UIColor* color = [self createRandomColor]; //b/c we're calling this method from inside this view controller, use []
+    UIView* newBlock =[self createBlock:color];
+    
+    NSInteger randStart = arc4random_uniform(5); //generate a random start position
+    NSInteger randEnd = arc4random_uniform(5); //generate a random end position
+    
+    //Main view: create this block, doesn't show until added in subview
+    newBlock.frame=CGRectMake(kBlockSize*randStart, 0.0f, kBlockSize, kBlockSize);//(x pos, y pos, width, height)
+    
+    [self.view addSubview:newBlock]; //add block to main view (show block)
+    
+    //animate the block
+    //CGFloat is a primitive type, so it doesn't need a *, because it doesn't need a pointer
+    CGFloat newBlockOriginX = randEnd*CGRectGetWidth(newBlock.frame);
+    CGFloat newBlockOriginY = CGRectGetHeight(newBlock.superview.bounds)-level*CGRectGetHeight(newBlock.frame);
+    
+    
+    
+    [UIView animateWithDuration:10.0f
+                          delay:0.5f
+                        options:UIViewAnimationOptionCurveLinear
+                     animations:^()
+                                    {
+                                        newBlock.frame=CGRectMake(newBlockOriginX, newBlockOriginY, kBlockSize, kBlockSize);
+                                    }
+                                        completion:^(BOOL finished) {
+                         
+                         NSLog(@"dropped block"); //@ for any NS object //ask J about this
+                         //NSLog(@"dropped block", frameHeight);
+                     }
+     ];
+}
 @end
 
 
