5724656b743bbfc1a9dd3f9b2321e140fac97528
joyjding <joy.j.ding@gmail.com>
2013-09-07 14:58:01 -0700

    set up for welcome screen

---
+++ 10KTimer/10KTimer/JDWelcomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  JDWelcomeViewController.h
+//  10KTimer
+//
+//  Created by Joy Ding on 9/7/13.
+//  Copyright (c) 2013 Hackbright. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JDWelcomeViewController : UIViewController
+
+@end
---
+++ 10KTimer/10KTimer/JDWelcomeViewController.m
@@ -0,0 +1,39 @@
+//
+//  JDWelcomeViewController.m
+//  10KTimer
+//
+//  Created by Joy Ding on 9/7/13.
+//  Copyright (c) 2013 Hackbright. All rights reserved.
+//
+
+#import "JDWelcomeViewController.h"
+
+@interface JDWelcomeViewController ()
+@property (strong, nonatomic) IBOutlet UIButton *welcomeStartTiming;
+@property (strong, nonatomic) IBOutlet UILabel *welcomeTotalTime;
+@end
+
+@implementation JDWelcomeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
